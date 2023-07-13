ef2fa19cb84dda31ba17495ad21ac6aead14853e
naveennk01 <125952574+naveennk01@users.noreply.github.com>
2023-07-13 11:35:28 +0530

    Add files via upload

---
+++ src/components/Pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../about.css";
+
+export const About = () => {
+  return (
+    <div class="about">
+      <h3>About</h3>
+      
+    </div>
+  );
+};
---
+++ src/components/Pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import rohitsha from "./rohitsha.jpg";
+export const Home = () => {
+  return (
+    <div style={{ backgroundImage:`url(${rohitsha})`,backgroundRepeat:"no-repeat",backgroundSize:"contain"}}>
+    
+    </div>
+  );
+};
